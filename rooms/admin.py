8565b0746854c91d475c6c0e3cd1c82ee2f62e97
tigerish009 <tigerish009@gmail.com>
2021-01-20 21:25:44 +0900

    room_type_211020

--- rooms/admin.py
+++ rooms/admin.py
@@ -1,7 +1,14 @@
 from django.contrib import admin
 from . import models
 
+
 # Register your models here.
+
+
+@admin.register(models.RoomType)
+class ItemAdmin(admin.ModelAdmin):
+
+    pass
 
 
 @admin.register(models.Room)
